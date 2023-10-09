52c52d9f5b0b836a76cab20b89262943705a2a9a
Amirul Islam <amirul@mislbd.com>
2023-10-09 16:13:51 +0600

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:common_button/common_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -79,22 +80,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: CommonButton(
+                onPress: _incrementCounter,
+                text: "Counter",
+                textAlign: Alignment.center,
+                tColor: Colors.white,
+                bgColor: Colors.orange,
+                isIcon: false,
+                customShape: 1,
+              ),
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
